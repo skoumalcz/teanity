fcf038f1cca71b47b14ff4e4e12ce2b83fcbaf2d
Jan Bína <binajohny@gmail.com>
2018-06-21 01:46:35 +0200

    Add CompositeDisposable to BaseViewModel

--- app/src/main/java/com/skoumal/teanity/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/skoumal/teanity/ui/base/BaseViewModel.kt
@@ -4,14 +4,25 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.skoumal.teanity.ui.events.ViewEvent
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 abstract class BaseViewModel : ViewModel() {
 
+    private val disposables = CompositeDisposable()
     private val _viewEvents = MutableLiveData<ViewEvent>()
     val viewEvents: LiveData<ViewEvent> get() = _viewEvents
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
 
     fun <Event : ViewEvent> Event.publish() {
         _viewEvents.value = this
     }
 
+    fun Disposable.add() {
+        disposables.add(this)
+    }
 }
