a3eb0745aa4c293f62370ed81a1e3996a3f0c5da
Viktor De Pasquale <neytireyqa@gmail.com>
2018-06-20 20:49:40 +0200

    Add BaseDao   Can be implemented by top-level DAO allowing boilerplate reduction and fast plug-in utility

---
+++ app/src/main/java/com/skoumal/teanity/data/database/BaseDao.kt
@@ -0,0 +1,41 @@
+package com.skoumal.teanity.data.database
+
+import android.arch.persistence.room.*
+
+interface BaseDao<T> {
+
+    @Delete
+    fun delete(obj: T)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(obj: T): Long
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(objects: List<T>): List<Long>
+
+    @Update
+    fun update(obj: T): Int
+
+    @Transaction
+    @Update
+    fun update(objects: List<T>): Int
+
+    /**
+     * Requires proper annotation that deletes the data. If not overridden in DAO it's rendering
+     * itself useless.
+     *
+     * Example:
+     * `@Query("DELETE FROM channel")`
+     */
+    fun deleteAll()
+
+    /**
+     * Requires proper annotation that fetches the data. If not overridden in DAO it's rendering
+     * itself useless.
+     *
+     * Example:
+     * `@Query("SELECT * FROM channel")`
+     */
+    fun fetchAll(): List<T>
+}
