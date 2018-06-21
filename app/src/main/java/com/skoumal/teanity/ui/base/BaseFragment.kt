1d849b77bd48c450994fe44a1fc13d176fb5a34b
Jan Bína <binajohny@gmail.com>
2018-06-21 12:08:10 +0200

    Add save/restore state methods to base classes

--- app/src/main/java/com/skoumal/teanity/ui/base/BaseFragment.kt
+++ app/src/main/java/com/skoumal/teanity/ui/base/BaseFragment.kt
@@ -2,11 +2,13 @@ package com.skoumal.teanity.ui.base
 
 import android.databinding.ViewDataBinding
 import android.os.Bundle
+import android.support.annotation.CallSuper
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import com.evernote.android.state.StateSaver
 import com.skoumal.teanity.BR
 import com.skoumal.teanity.ui.events.ViewEvent
 import com.skoumal.teanity.ui.events.ViewEventObserver
@@ -26,6 +28,8 @@ abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewDataBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        restoreState(savedInstanceState)
 
         viewModel.viewEvents.observe(this, viewEventObserver)
     }
@@ -51,7 +55,34 @@ abstract class BaseFragment<ViewModel : BaseViewModel, Binding : ViewDataBinding
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        saveState(outState)
+    }
+
     open fun onEventDispatched(event: ViewEvent) {}
 
     open fun Binding.unbindViews() {}
+
+    /**
+     * Override this method if you have more viewModels or anything else you want to restore
+     * You should also override [saveState]
+     */
+    @CallSuper
+    open fun restoreState(savedInstanceState: Bundle?) {
+        StateSaver.restoreInstanceState(this, savedInstanceState)
+        viewModel.restoreState(savedInstanceState)
+    }
+
+    /**
+     * Override this method if you have more viewModels or anything else you want to save
+     * You should also override [restoreState]
+     */
+    @CallSuper
+    open fun saveState(outState: Bundle) {
+        StateSaver.saveInstanceState(this, outState);
+        viewModel.saveState(outState)
+    }
+
 }
