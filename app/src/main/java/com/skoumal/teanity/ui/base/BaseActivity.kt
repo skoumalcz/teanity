60b29aeee26c0b659f4667b7b6a820ba1b4a14dd
Jan Bína <binajohny@gmail.com>
2018-06-21 12:08:10 +0200

    Add save/restore state methods to base classes

--- app/src/main/java/com/skoumal/teanity/ui/base/BaseActivity.kt
+++ app/src/main/java/com/skoumal/teanity/ui/base/BaseActivity.kt
@@ -2,8 +2,10 @@ package com.skoumal.teanity.ui.base
 
 import android.databinding.ViewDataBinding
 import android.os.Bundle
+import android.support.annotation.CallSuper
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.NavController
+import com.evernote.android.state.StateSaver
 import com.skoumal.teanity.BR
 import com.skoumal.teanity.ui.events.ViewEvent
 import com.skoumal.teanity.ui.events.ViewEventObserver
@@ -25,6 +27,8 @@ abstract class BaseActivity<ViewModel : BaseViewModel, Binding : ViewDataBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        restoreState(savedInstanceState)
+
         binding = setBindingContentView<Binding>(layoutRes).apply {
             setVariable(BR.viewModel, this@BaseActivity.viewModel)
         }
@@ -40,8 +44,36 @@ abstract class BaseActivity<ViewModel : BaseViewModel, Binding : ViewDataBinding
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        if (outState != null) {
+            saveState(outState)
+        }
+    }
+
     open fun onEventDispatched(event: ViewEvent) {}
 
     open fun Binding.unbindViews() {}
+
+    /**
+     * Override this method if you have more viewModels or anything else you want to restore
+     * You should also override [saveState]
+     */
+    @CallSuper
+    open fun restoreState(savedInstanceState: Bundle?) {
+        StateSaver.restoreInstanceState(this, savedInstanceState)
+        viewModel.restoreState(savedInstanceState)
+    }
+
+    /**
+     * Override this method if you have more viewModels or anything else you want to save
+     * You should also override [restoreState]
+     */
+    @CallSuper
+    open fun saveState(outState: Bundle) {
+        StateSaver.saveInstanceState(this, outState);
+        viewModel.saveState(outState)
+    }
 
 }
--- app/src/main/java/com/skoumal/teanity/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/skoumal/teanity/ui/base/BaseViewModel.kt
@@ -3,6 +3,8 @@ package com.skoumal.teanity.ui.base
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.os.Bundle
+import com.evernote.android.state.StateSaver
 import com.skoumal.teanity.ui.events.ViewEvent
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
@@ -16,6 +18,14 @@ abstract class BaseViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         disposables.clear()
+    }
+
+    fun restoreState(savedInstanceState: Bundle?) {
+        StateSaver.restoreInstanceState(this, savedInstanceState)
+    }
+
+    fun saveState(outState: Bundle) {
+        StateSaver.saveInstanceState(this, outState);
     }
 
     fun <Event : ViewEvent> Event.publish() {
