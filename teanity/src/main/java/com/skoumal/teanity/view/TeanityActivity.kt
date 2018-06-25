60486f343c1c89da4e15104d5c275d2e4c12041d
Jan Bína <binajohny@gmail.com>
2018-06-26 00:39:22 +0200

    Add TeanityView interface
    It contains common functionality of TeanityActivity
    and TeanityFragment

--- teanity/src/main/java/com/skoumal/teanity/view/TeanityActivity.kt
+++ teanity/src/main/java/com/skoumal/teanity/view/TeanityActivity.kt
@@ -3,18 +3,16 @@ package com.skoumal.teanity.view
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.os.Bundle
-import android.support.annotation.CallSuper
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.NavController
-import com.evernote.android.state.StateSaver
 import com.skoumal.teanity.BR
 import com.skoumal.teanity.viewevents.SimpleViewEvent
-import com.skoumal.teanity.viewevents.ViewEvent
 import com.skoumal.teanity.viewevents.ViewEventObserver
 import com.skoumal.teanity.viewmodel.TeanityViewModel
 
 abstract class TeanityActivity<ViewModel : TeanityViewModel, Binding : ViewDataBinding> :
-    AppCompatActivity() {
+    AppCompatActivity(),
+    TeanityView<Binding> {
 
     protected lateinit var binding: Binding
     protected abstract val layoutRes: Int
@@ -55,40 +53,13 @@ abstract class TeanityActivity<ViewModel : TeanityViewModel, Binding : ViewDataB
         }
     }
 
-    /**
-     * Called for all [ViewEvent]s published by associated viewModel.
-     * For [SimpleViewEvent]s, both this and [onSimpleEventDispatched]
-     * methods are called - you can choose the way how you handle them.
-     */
-    open fun onEventDispatched(event: ViewEvent) {}
-
-    /**
-     * Called for all [SimpleViewEvent]s published by associated viewModel.
-     * Both this and [onEventDispatched] methods are called - you can choose
-     * the way how you handle them.
-     */
-    open fun onSimpleEventDispatched(event: Int) {}
-
-    open fun Binding.unbindViews() {}
-
-    /**
-     * Override this method if you have more viewModels or anything else you want to restore
-     * You should also override [saveState]
-     */
-    @CallSuper
-    open fun restoreState(savedInstanceState: Bundle?) {
-        StateSaver.restoreInstanceState(this, savedInstanceState)
-        viewModel.restoreState(savedInstanceState)
-    }
-
-    /**
-     * Override this method if you have more viewModels or anything else you want to save
-     * You should also override [restoreState]
-     */
-    @CallSuper
-    open fun saveState(outState: Bundle) {
-        StateSaver.saveInstanceState(this, outState);
+    override fun saveState(outState: Bundle) {
+        super.saveState(outState)
         viewModel.saveState(outState)
     }
 
+    override fun restoreState(savedInstanceState: Bundle?) {
+        super.restoreState(savedInstanceState)
+        viewModel.restoreState(savedInstanceState)
+    }
 }
---
+++ teanity/src/main/java/com/skoumal/teanity/view/TeanityView.kt
@@ -0,0 +1,44 @@
+package com.skoumal.teanity.view
+
+import android.os.Bundle
+import android.support.annotation.CallSuper
+import com.evernote.android.state.StateSaver
+import com.skoumal.teanity.viewevents.SimpleViewEvent
+import com.skoumal.teanity.viewevents.ViewEvent
+
+internal interface TeanityView<Binding> {
+
+    /**
+     * Called for all [ViewEvent]s published by associated viewModel.
+     * For [SimpleViewEvent]s, both this and [onSimpleEventDispatched]
+     * methods are called - you can choose the way how you handle them.
+     */
+    fun onEventDispatched(event: ViewEvent) {}
+
+    /**
+     * Called for all [SimpleViewEvent]s published by associated viewModel.
+     * Both this and [onEventDispatched] methods are called - you can choose
+     * the way how you handle them.
+     */
+    fun onSimpleEventDispatched(event: Int) {}
+
+    fun Binding.unbindViews() {}
+
+    /**
+     * Override this method if you have more viewModels or anything else you want to restore
+     * You should also override [saveState]
+     */
+    @CallSuper
+    fun restoreState(savedInstanceState: Bundle?) {
+        StateSaver.restoreInstanceState(this, savedInstanceState)
+    }
+
+    /**
+     * Override this method if you have more viewModels or anything else you want to save
+     * You should also override [restoreState]
+     */
+    @CallSuper
+    fun saveState(outState: Bundle) {
+        StateSaver.saveInstanceState(this, outState)
+    }
+}
