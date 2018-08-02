f563da78d39a92e34e6ad1f31bdbcce3afd23e95
Jan Bína <binajohny@gmail.com>
2018-08-03 00:06:38 +0200

    Add divider*Margin attributes

--- teanity/src/main/java/com/skoumal/teanity/extensions/Context.kt
+++ teanity/src/main/java/com/skoumal/teanity/extensions/Context.kt
@@ -1,9 +1,25 @@
 package com.skoumal.teanity.extensions
 
 import android.content.Context
+import android.os.Build
+import android.view.View
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 
 fun Context.colorCompat(@ColorRes id: Int) = ContextCompat.getColor(this, id)
+
 fun Context.drawableCompat(@DrawableRes id: Int) = ContextCompat.getDrawable(this, id)
+
+/**
+ * Pass [start] and [end] dimensions, function will return left and right
+ * with respect to RTL layout direction
+ */
+fun Context.startEndToLeftRight(start: Int, end: Int): Pair<Int, Int> {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 &&
+        resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL
+    ) {
+        return end to start
+    }
+    return start to end
+}
