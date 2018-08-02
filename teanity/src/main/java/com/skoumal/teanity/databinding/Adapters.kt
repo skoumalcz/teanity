95a8cac11ca26ec536a5aabd15e4c7dd3992c501
Jan Bína <binajohny@gmail.com>
2018-08-02 16:26:14 +0200

    Add Binding adapter for recyclerview divider

--- teanity/src/main/java/com/skoumal/teanity/databinding/Adapters.kt
+++ teanity/src/main/java/com/skoumal/teanity/databinding/Adapters.kt
@@ -1,8 +1,13 @@
 package com.skoumal.teanity.databinding
 
-import androidx.databinding.BindingAdapter
+import android.graphics.drawable.GradientDrawable
+import android.util.Log
 import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.request.RequestOptions
+import com.skoumal.teanity.extensions.toPx
+import com.skoumal.teanity.util.KItemDecoration
 
 @BindingAdapter("gone")
 fun setGone(view: View, gone: Boolean) {
@@ -29,4 +34,21 @@ fun RequestOptions.applyTransformation(transformation: Int): RequestOptions = wh
     Transformations.FIT_CENTER -> fitCenter()
     Transformations.CIRCLE_CROP -> circleCrop()
     else -> throw IllegalArgumentException("Unsupported transformation")
+}
+
+@BindingAdapter("dividerColor", "dividerHorizontal", "dividerSize", requireAll = false)
+fun setDivider(view: RecyclerView, dividerColor: Int, dividerHorizontal: Boolean, _dividerSize: Int?) {
+
+    val orientation = if (dividerHorizontal) RecyclerView.HORIZONTAL else RecyclerView.VERTICAL
+    val dividerSize = _dividerSize ?: 1.toPx()
+    val (width, height) = if (dividerHorizontal) dividerSize to 1 else 1 to dividerSize
+
+    val drawable = GradientDrawable().apply {
+        setSize(width, height)
+        shape = GradientDrawable.RECTANGLE
+        setColor(dividerColor)
+    }
+
+    val decoration = KItemDecoration(view.context, orientation).setDeco(drawable)
+    view.addItemDecoration(decoration)
 }
