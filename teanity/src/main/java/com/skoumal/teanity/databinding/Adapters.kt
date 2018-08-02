47f9b2dc1c2bf9cdd971475c891fe8680a17e558
Jan Bína <binajohny@gmail.com>
2018-08-03 00:06:38 +0200

    Add divider*Margin attributes

--- teanity/src/main/java/com/skoumal/teanity/databinding/Adapters.kt
+++ teanity/src/main/java/com/skoumal/teanity/databinding/Adapters.kt
@@ -1,12 +1,15 @@
 package com.skoumal.teanity.databinding
 
 import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.InsetDrawable
 import android.view.View
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.request.RequestOptions
+import com.skoumal.teanity.extensions.startEndToLeftRight
 import com.skoumal.teanity.extensions.toPx
 import com.skoumal.teanity.util.KItemDecoration
+import kotlin.math.roundToInt
 
 @BindingAdapter("gone")
 fun setGone(view: View, gone: Boolean) {
@@ -35,24 +38,45 @@ fun RequestOptions.applyTransformation(transformation: Int): RequestOptions = wh
     else -> throw IllegalArgumentException("Unsupported transformation")
 }
 
-@BindingAdapter("dividerColor", "dividerHorizontal", "dividerSize", "dividerAfterLast", requireAll = false)
+@BindingAdapter(
+    "dividerColor",
+    "dividerHorizontal",
+    "dividerSize",
+    "dividerAfterLast",
+    "dividerMarginStart",
+    "dividerMarginEnd",
+    "dividerMarginTop",
+    "dividerMarginBottom",
+    requireAll = false
+)
 fun setDivider(
     view: RecyclerView,
     color: Int,
     horizontal: Boolean,
-    _size: Int?,
-    _afterLast: Boolean?
+    _size: Float,
+    _afterLast: Boolean?,
+    marginStartF: Float,
+    marginEndF: Float,
+    marginTopF: Float,
+    marginBottomF: Float
 ) {
-
     val orientation = if (horizontal) RecyclerView.HORIZONTAL else RecyclerView.VERTICAL
-    val size = _size ?: 1.toPx()
+    val size = if (_size > 0) _size.roundToInt() else 1.toPx()
     val (width, height) = if (horizontal) size to 1 else 1 to size
     val afterLast = _afterLast ?: true
+
+    val marginStart = marginStartF.roundToInt()
+    val marginEnd = marginEndF.roundToInt()
+    val marginTop = marginTopF.roundToInt()
+    val marginBottom = marginBottomF.roundToInt()
+    val (marginLeft, marginRight) = view.context.startEndToLeftRight(marginStart, marginEnd)
 
     val drawable = GradientDrawable().apply {
         setSize(width, height)
         shape = GradientDrawable.RECTANGLE
         setColor(color)
+    }.let {
+        InsetDrawable(it, marginLeft, marginTop, marginRight, marginBottom)
     }
 
     val decoration = KItemDecoration(view.context, orientation)
