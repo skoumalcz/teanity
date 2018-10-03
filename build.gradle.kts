c7e5255eae9a934908a8f4ecc0f56037eac509d9
Viktor De Pasquale <neytireyqa@gmail.com>
2018-10-03 11:07:00 +0200

    Updated libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 buildscript {
-    extra.set("versions", mapOf(
+    extra.set(
+        "versions", mapOf(
             /* === LIB === */
-            "kotlin" to "1.2.70",
+            "kotlin" to "1.2.71",
             "supportlib" to "1.0.0",
             "room" to "2.0.0-rc01",
             "lifecycle" to "2.0.0-rc01",
@@ -10,21 +11,22 @@ buildscript {
             "glide" to "4.8.0-SNAPSHOT",
             "state" to "1.3.1",
             "rxkotlin" to "2.2.0",
-            "rxandroid" to "2.0.2",
-            "dexter" to "4.2.0",
-            "ktx" to "0.3",
+            "rxandroid" to "2.1.0",
+            "dexter" to "5.0.0",
+            "ktx" to "1.0.0-alpha1",
 
             /* === APP === */
             "playServices" to "15.0.1",
             "constraintLayout" to "2.0.0-alpha2",
             "gradlePlugin" to "3.3.0-alpha03",
-            "koin" to "1.0.0-beta-3",
+            "koin" to "1.0.1",
             "retrofit" to "2.4.0",
-            "okhttp" to "3.10.0",
-            "moshi" to "1.6.0",
+            "okhttp" to "3.11.0",
+            "moshi" to "1.7.0",
             "crashlytics" to "2.9.3",
             "fabric" to "1.25.4"
-    ))
+        )
+    )
 
     repositories {
         google()
