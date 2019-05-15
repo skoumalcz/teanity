90fcfc6c087baf7591ab4293266baae825a3a90d
Viktor De Pasquale <info@depasquale.wiki>
2019-05-15 13:16:24 +0200

    Updated ComparableRvItem to be (erased)type safe and versatile

--- teanity/src/main/java/com/skoumal/teanity/databinding/RecyclerViewItems.kt
+++ teanity/src/main/java/com/skoumal/teanity/databinding/RecyclerViewItems.kt
@@ -3,9 +3,13 @@ package com.skoumal.teanity.databinding
 import androidx.annotation.CallSuper
 import androidx.databinding.ViewDataBinding
 import com.skoumal.teanity.BR
+import com.skoumal.teanity.util.ComparableEntity
 import com.skoumal.teanity.util.DiffObservableList
 import me.tatarka.bindingcollectionadapter2.ItemBinding
+import me.tatarka.bindingcollectionadapter2.OnItemBind
 
+abstract class GenericRvItem : ComparableRvItem<GenericRvItem>()
+abstract class ComparableRvItem<in T> : RvItem(), ComparableEntity<T>
 abstract class RvItem {
 
     abstract val layoutRes: Int
@@ -23,26 +27,25 @@ abstract class RvItem {
     open fun onBindingBound(binding: ViewDataBinding) {}
 }
 
-abstract class ComparableRvItem<in T> : RvItem() {
+//region Binding
 
-    abstract fun itemSameAs(other: T): Boolean
-    abstract fun contentSameAs(other: T): Boolean
-    @Suppress("UNCHECKED_CAST")
-    open fun genericItemSameAs(other: Any): Boolean = other::class == this::class && itemSameAs(other as T)
-    @Suppress("UNCHECKED_CAST")
-    open fun genericContentSameAs(other: Any): Boolean = other::class == this::class && contentSameAs(other as T)
-
-    companion object {
-        val callback = object : DiffObservableList.Callback<ComparableRvItem<*>> {
-            override fun areItemsTheSame(
-                oldItem: ComparableRvItem<*>,
-                newItem: ComparableRvItem<*>
-            ) = oldItem.genericItemSameAs(newItem)
-
-            override fun areContentsTheSame(
-                oldItem: ComparableRvItem<*>,
-                newItem: ComparableRvItem<*>
-            ) = oldItem.genericContentSameAs(newItem)
-        }
-    }
+inline fun <T : RvItem> bindingOf(crossinline init: (ItemBinding<*>) -> Unit) = OnItemBind<T> { itemBinding, _, item ->
+    item.bind(itemBinding)
+    init(itemBinding)
 }
+
+//endregion
+//region DiffList
+
+fun <T : ComparableEntity<T>> diffListOf() = DiffObservableList<T>(comparableCallback())
+internal fun <T : ComparableEntity<T>> comparableCallback() = object : DiffObservableList.Callback<T> {
+    override fun areContentsTheSame(oldItem: T, newItem: T) = oldItem.contentSameAs(newItem)
+    override fun areItemsTheSame(oldItem: T, newItem: T) = oldItem.sameAs(newItem)
+}
+
+//endregion
+//region Helper
+
+inline fun <reified T> Any.compareToSafe(eval: (T) -> Boolean) = (this as? T)?.let(eval) ?: false
+
+//endregion
