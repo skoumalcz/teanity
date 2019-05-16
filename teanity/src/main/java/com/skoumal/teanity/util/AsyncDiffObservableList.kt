9bc3f164cce26f75413708f4b1722d3086049db6
Viktor De Pasquale <info@depasquale.wiki>
2019-05-16 08:54:50 +0200

    Fixed async list to queue all incoming changes instead of throwing them away

--- teanity/src/main/java/com/skoumal/teanity/util/AsyncDiffObservableList.kt
+++ teanity/src/main/java/com/skoumal/teanity/util/AsyncDiffObservableList.kt
@@ -1,17 +1,19 @@
 package com.skoumal.teanity.util
 
 import androidx.recyclerview.widget.DiffUtil
+import com.skoumal.teanity.extensions.applySchedulers
 import com.skoumal.teanity.extensions.subscribeK
 import io.reactivex.Single
-import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
 
 /**
  * Default asynchronous implementation of [BaseDiffObservableList]. Execution of all diff calculations is strictly on
  * background thread.
  *
- * When populating list with new values, the list will scrap all computation currently running and **will not**
- * dispatch previous changes to listeners. This is done in order to minimize number of animations running at once when
- * list is getting rapidly updated.
+ * When populating list with new values, the list will queue all computation currently running and **will**
+ * consecutively dispatch previous changes to listeners.
+ *
+ * In case of an error the queue will be emptied and re-started.
  *
  * @see BaseDiffObservableList
  * @inherit [BaseDiffObservableList]
@@ -21,14 +23,76 @@ open class AsyncDiffObservableList<T>(
     detectMoves: Boolean = true
 ) : BaseDiffObservableList<T>(callback, detectMoves) {
 
-    private var runningTask: Disposable? = null
+    private var dispatchQueue = PublishSubject.create<Single<DiffUtil.DiffResult>>()
+    private var pendingList = mutableListOf<T>()
 
-    val isPendingChange get() = runningTask?.isDisposed?.not() ?: false
-
-    override fun awaitDifferenceFrom(oldItems: List<T>, newItems: List<T>, callback: (DiffUtil.DiffResult) -> Unit) {
-        runningTask?.dispose()
-        runningTask = Single.just(oldItems to newItems)
-            .map { doCalculateDiff(oldItems, newItems) }
-            .subscribeK { callback(it) }
+    init {
+        dispatchQueue
+            .onErrorResumeNext { throwable: Throwable ->
+                throwable.printStackTrace()
+                dispatchQueue = PublishSubject.create()
+                dispatchQueue
+            }
+            .flatMapSingle { it }
+            .subscribeK()
     }
+
+    @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+    override fun awaitDifferenceFrom(ignored: List<T>, newItems: List<T>, callback: (DiffUtil.DiffResult) -> Unit) {
+        val task = Single.just(newItems)
+            .map {
+                val old = synchronized(listLock) { list.toList() }
+                doCalculateDiff(old, it)
+            }
+            .applySchedulers()
+            .doOnSuccess(callback)
+        dispatchQueue.onNext(task)
+    }
+
+    override fun add(element: T): Boolean {
+        val result = pendingList.add(element)
+        update(pendingList)
+        return result
+    }
+
+    override fun add(index: Int, element: T) {
+        pendingList.add(index, element)
+        update(pendingList)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        val result = pendingList.addAll(elements)
+        update(pendingList)
+        return result
+    }
+
+    override fun addAll(index: Int, elements: Collection<T>): Boolean {
+        val result = pendingList.addAll(index, elements)
+        update(pendingList)
+        return result
+    }
+
+    override fun clear() {
+        pendingList.clear()
+        update(pendingList)
+    }
+
+    override fun remove(element: T): Boolean {
+        val result = pendingList.remove(element)
+        update(pendingList)
+        return result
+    }
+
+    override fun removeAt(index: Int): T {
+        val result = pendingList.removeAt(index)
+        update(pendingList)
+        return result
+    }
+
+    override fun set(index: Int, element: T): T {
+        val result = pendingList.set(index, element)
+        update(pendingList)
+        return result
+    }
+
 }
