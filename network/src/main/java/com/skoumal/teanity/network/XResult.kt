bb78615d0fb56cca0f84257c268b09a7c252766d
viktor de pasquale <info@depasquale.wiki>
2019-09-02 11:36:47 +0200

    Fixed implicit casting would cause exception

--- network/src/main/java/com/skoumal/teanity/network/XResult.kt
+++ network/src/main/java/com/skoumal/teanity/network/XResult.kt
@@ -2,10 +2,10 @@ package com.skoumal.teanity.network
 
 inline fun <T, R> Result<List<T>>.mapList(mapper: (T) -> R) = when {
     isSuccess -> (getOrNull()?.map(mapper) ?: listOf()).let { Result.success(it) }
-    else -> this as Result<List<R>> // We don't give a f*. It's an exception anyways.
+    else -> Result.failure(exceptionOrNull() ?: IllegalStateException("No exception was provided"))
 }
 
 inline fun <T, R> Result<List<T>>.flatMap(mapper: (T) -> List<R>) = when {
     isSuccess -> (getOrNull()?.map(mapper) ?: listOf()).let { Result.success(it.flatten()) }
-    else -> this as Result<List<R>> // We don't give a f*. It's an exception anyways.
+    else -> Result.failure(exceptionOrNull() ?: IllegalStateException("No exception was provided"))
 }
