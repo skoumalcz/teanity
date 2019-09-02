bb954d5c372cdc22a5c797e5f355e01aeb41e236
viktor de pasquale <info@depasquale.wiki>
2019-09-02 11:18:46 +0200

    Updated usage to explicit extension

--- di/src/main/java/com/skoumal/teanity/di/module/Generics.kt
+++ di/src/main/java/com/skoumal/teanity/di/module/Generics.kt
@@ -3,7 +3,6 @@ package com.skoumal.teanity.di.module
 import android.app.*
 import android.app.job.JobScheduler
 import android.app.usage.NetworkStatsManager
-import android.content.Context
 import android.location.LocationManager
 import android.media.AudioManager
 import android.media.MediaRouter
@@ -17,32 +16,33 @@ import android.view.LayoutInflater
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.getSystemService
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 internal val genericModule = module {
     /* Resources */
-    single { get<Context>().resources }
-    single { get<Context>().assets }
+    single { androidContext().resources }
+    single { androidContext().assets }
 
     /* All available system services */
-    single { get<Context>().getSystemService<WindowManager>() }
-    single { get<Context>().getSystemService<LayoutInflater>() }
-    single { get<Context>().getSystemService<ActivityManager>() }
-    single { get<Context>().getSystemService<NotificationManager>() }
-    single { get<Context>().getSystemService<KeyguardManager>() }
-    single { get<Context>().getSystemService<LocationManager>() }
-    single { get<Context>().getSystemService<SearchManager>() }
-    single { get<Context>().getSystemService<Vibrator>() }
-    single { get<Context>().getSystemService<ConnectivityManager>() }
-    single { get<Context>().getSystemService<WifiManager>() }
-    single { get<Context>().getSystemService<AudioManager>() }
-    single { get<Context>().getSystemService<MediaRouter>() }
-    single { get<Context>().getSystemService<TelephonyManager>() }
-    single { get<Context>().getSystemService<SubscriptionManager>() }
-    single { get<Context>().getSystemService<InputMethodManager>() }
-    single { get<Context>().getSystemService<UiModeManager>() }
-    single { get<Context>().getSystemService<DownloadManager>() }
-    single { get<Context>().getSystemService<BatteryManager>() }
-    single { get<Context>().getSystemService<JobScheduler>() }
-    single { get<Context>().getSystemService<NetworkStatsManager>() }
+    single { androidContext().getSystemService<WindowManager>() }
+    single { androidContext().getSystemService<LayoutInflater>() }
+    single { androidContext().getSystemService<ActivityManager>() }
+    single { androidContext().getSystemService<NotificationManager>() }
+    single { androidContext().getSystemService<KeyguardManager>() }
+    single { androidContext().getSystemService<LocationManager>() }
+    single { androidContext().getSystemService<SearchManager>() }
+    single { androidContext().getSystemService<Vibrator>() }
+    single { androidContext().getSystemService<ConnectivityManager>() }
+    single { androidContext().getSystemService<WifiManager>() }
+    single { androidContext().getSystemService<AudioManager>() }
+    single { androidContext().getSystemService<MediaRouter>() }
+    single { androidContext().getSystemService<TelephonyManager>() }
+    single { androidContext().getSystemService<SubscriptionManager>() }
+    single { androidContext().getSystemService<InputMethodManager>() }
+    single { androidContext().getSystemService<UiModeManager>() }
+    single { androidContext().getSystemService<DownloadManager>() }
+    single { androidContext().getSystemService<BatteryManager>() }
+    single { androidContext().getSystemService<JobScheduler>() }
+    single { androidContext().getSystemService<NetworkStatsManager>() }
 }
