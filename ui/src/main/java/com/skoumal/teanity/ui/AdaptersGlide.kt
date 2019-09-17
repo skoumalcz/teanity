7c7e8be3e5425dbf5e3a54fa42670754ad2a1ef3
Viktor De Pasquale <info@depasquale.wiki>
2019-09-17 09:02:21 +0200

    Updated transformations so native implementation (ImageView) can be used

--- ui/src/main/java/com/skoumal/teanity/ui/AdaptersGlide.kt
+++ ui/src/main/java/com/skoumal/teanity/ui/AdaptersGlide.kt
@@ -1,7 +1,9 @@
 package com.skoumal.teanity.ui
 
+import android.widget.ImageView
 import com.bumptech.glide.request.RequestOptions
 
+@Deprecated("")
 object Transformations {
     const val NONE = 0
     const val CENTER_CROP = 1
@@ -10,11 +12,61 @@ object Transformations {
     const val CIRCLE_CROP = 4
 }
 
-fun RequestOptions.applyTransformation(transformation: Int): RequestOptions = when (transformation) {
+@Deprecated("Use transformation by ImageView.ScaleType, ScaleType or String")
+@Suppress("DEPRECATION")
+fun RequestOptions.applyTransformation(transform: Int): RequestOptions = when (transform) {
     Transformations.NONE -> this
     Transformations.CENTER_CROP -> centerCrop()
     Transformations.CENTER_INSIDE -> centerInside()
     Transformations.FIT_CENTER -> fitCenter()
     Transformations.CIRCLE_CROP -> circleCrop()
     else -> throw IllegalArgumentException("Unsupported transformation")
+}
+
+enum class ScaleType(val text: String) {
+    CIRCLE_CROP("circleCrop"),
+    CENTER_CROP("centerCrop"),
+    CENTER_INSIDE("centerInside"),
+    FIT_CENTER("fitCenter"),
+
+    @Deprecated("Not supported.")
+    FIT_END("fitEnd"),
+    @Deprecated("Not supported.")
+    FIT_START("fitStart"),
+    @Deprecated("Not supported.")
+    FIT_XY("fitXY"),
+    @Deprecated("Not supported.")
+    MATRIX("matrix"),
+    @Deprecated("Not supported.")
+    CENTER("center")
+}
+
+fun RequestOptions.applyTransformation(scaleType: ImageView.ScaleType) =
+    applyTransformation(scaleType.asInternal())
+
+fun RequestOptions.applyTransformation(scaleType: ScaleType) =
+    applyTransformation(scaleType.text)
+
+@Suppress("DEPRECATION")
+fun RequestOptions.applyTransformation(scaleType: String): RequestOptions = when (scaleType) {
+    ScaleType.CIRCLE_CROP.text -> circleCrop()
+    ScaleType.CENTER_CROP.text -> centerCrop()
+    ScaleType.CENTER_INSIDE.text -> centerInside()
+    ScaleType.FIT_CENTER.text -> fitCenter()
+
+    ScaleType.FIT_END.text, ScaleType.FIT_START.text, ScaleType.FIT_XY.text, ScaleType.MATRIX.text,
+    ScaleType.CENTER.text -> throw IllegalArgumentException("Scale type \"$scaleType\" is not supported.")
+
+    else -> throw IllegalArgumentException("Scale type \"$scaleType\" is not supported.")
+}
+
+private fun ImageView.ScaleType.asInternal() = when (this) {
+    ImageView.ScaleType.MATRIX -> ScaleType.MATRIX
+    ImageView.ScaleType.FIT_XY -> ScaleType.FIT_XY
+    ImageView.ScaleType.FIT_START -> ScaleType.FIT_START
+    ImageView.ScaleType.FIT_CENTER -> ScaleType.FIT_CENTER
+    ImageView.ScaleType.FIT_END -> ScaleType.FIT_END
+    ImageView.ScaleType.CENTER -> ScaleType.CENTER
+    ImageView.ScaleType.CENTER_CROP -> ScaleType.CENTER_CROP
+    ImageView.ScaleType.CENTER_INSIDE -> ScaleType.CENTER_INSIDE
 }
