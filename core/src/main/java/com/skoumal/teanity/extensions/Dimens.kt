3e969633174a38e4cbfc993fd0c8ddd65f8ad7a6
Viktor De Pasquale <info@depasquale.wiki>
2020-02-27 12:19:18 +0100

    Added dimen casts for float values

--- core/src/main/java/com/skoumal/teanity/extensions/Dimens.kt
+++ core/src/main/java/com/skoumal/teanity/extensions/Dimens.kt
@@ -4,6 +4,8 @@ import android.content.res.Resources
 import kotlin.math.ceil
 import kotlin.math.roundToInt
 
-fun Int.toDp(): Int = ceil(this / Resources.getSystem().displayMetrics.density).roundToInt()
+fun Int.toDp(): Int = ceil((this * 1.0f).toDp()).roundToInt()
+fun Int.toPx(): Int = (this * 1.0f).toPx().roundToInt()
 
-fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).roundToInt()
+fun Float.toDp() = this / Resources.getSystem().displayMetrics.density
+fun Float.toPx() = this * Resources.getSystem().displayMetrics.density
