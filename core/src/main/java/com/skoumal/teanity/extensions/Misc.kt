05ae775a27989de225a5cd2f718957b7a480f998
Viktor De Pasquale <info@depasquale.wiki>
2020-02-27 12:20:44 +0100

    Added deprecation for "ui" method

--- core/src/main/java/com/skoumal/teanity/extensions/Misc.kt
+++ core/src/main/java/com/skoumal/teanity/extensions/Misc.kt
@@ -2,5 +2,8 @@ package com.skoumal.teanity.extensions
 
 import android.os.Handler
 import android.os.Looper
+import com.skoumal.teanity.tools.annotation.RemoveOnDeprecation
 
+@Deprecated("Use coroutines with context = Dispatchers.Main")
+@RemoveOnDeprecation("1.2")
 fun ui(body: () -> Unit) = Handler(Looper.getMainLooper()).post(body)
