a1eae7084d463c66d08a70e3d75ab48511aac2b9
Viktor De Pasquale <info@depasquale.wiki>
2020-07-08 08:25:28 +0200

    Reverted deprecation for bind method in recycler view items

--- core/src/main/java/com/skoumal/teanity/databinding/RecyclerViewItems.kt
+++ core/src/main/java/com/skoumal/teanity/databinding/RecyclerViewItems.kt
@@ -1,8 +1,11 @@
 package com.skoumal.teanity.databinding
 
 import androidx.annotation.CallSuper
+import androidx.databinding.ObservableList
 import androidx.databinding.ViewDataBinding
 import com.skoumal.teanity.BR
+import com.skoumal.teanity.extensions.bindingOf
+import com.skoumal.teanity.list.BindingAdapter
 import com.skoumal.teanity.tools.annotation.RemoveOnDeprecation
 import com.skoumal.teanity.util.ComparableEntity
 import me.tatarka.bindingcollectionadapter2.ItemBinding
@@ -25,12 +28,12 @@ abstract class RecyclerViewItem : ComparableEntity<RecyclerViewItem> {
 
     abstract val layoutRes: Int
 
-    @Suppress("DeprecatedCallableAddReplaceWith")
-    @Deprecated(
-        "It's no longer suggested to use 3rd party libraries for binding to lists",
-        level = DeprecationLevel.WARNING
-    )
-    @RemoveOnDeprecation("1.3")
+    /**
+     * ## Definition
+     *
+     * Sets layout to the [BR.item] variable. It's invoked by [bindingOf] exclusively. You can call
+     * it yourself in the case when you're using [ObservableList] in conjunction with [ItemBinding].
+     * */
     @CallSuper
     open fun bind(binding: ItemBinding<*>) {
         binding.set(BR.item, layoutRes)
@@ -39,7 +42,8 @@ abstract class RecyclerViewItem : ComparableEntity<RecyclerViewItem> {
     /**
      * ## Definition
      *
-     * Pending bindings are immediately executed upon invoking `super.onBindingBound()`.
+     * Pending bindings are immediately executed upon invoking `super.onBindingBound()`. This
+     * function is invoked exclusively by [BindingAdapter].
      *
      * # Warning
      *
