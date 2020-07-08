68d972d685e9c506a7e2b75ccf30585d7d6e17e4
Viktor De Pasquale <info@depasquale.wiki>
2020-07-08 08:25:54 +0200

    Added deprecation warning for duplicate class

--- core/src/main/java/com/skoumal/teanity/databinding/BindingBoundAdapter.kt
+++ core/src/main/java/com/skoumal/teanity/databinding/BindingBoundAdapter.kt
@@ -1,8 +1,14 @@
 package com.skoumal.teanity.databinding
 
 import androidx.databinding.ViewDataBinding
+import com.skoumal.teanity.tools.annotation.RemoveOnDeprecation
 import me.tatarka.bindingcollectionadapter2.BindingRecyclerViewAdapter
 
+@Deprecated(
+    "Use BindingAdapter without explicit ItemBinding instead",
+    replaceWith = ReplaceWith("BindingAdapter", "com.skoumal.teanity.list.BindingAdapter")
+)
+@RemoveOnDeprecation("1.3")
 open class BindingBoundAdapter : BindingRecyclerViewAdapter<RecyclerViewItem>() {
 
     override fun onBindBinding(
